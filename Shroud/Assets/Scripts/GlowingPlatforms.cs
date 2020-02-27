377b5f68b36c445922df62bbb2dfe05e0312b3b0
Savvy Blaum <savannahmblaum@gmail.com>
2020-02-27 16:58:50 -0500

    2/27/20 Glowing platform halo thing started

---
+++ Shroud/Assets/Scripts/GlowingPlatforms.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingPlatforms : MonoBehaviour
+{
+    int glow = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*halo*//* = GetComponent<Halo>();*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if it's being stepped on, make the halo appear
+        if (glow > 0)
+        {
+            //halo.size = glow;
+            //glow--;
+        }
+    }
+}
